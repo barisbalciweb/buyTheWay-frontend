7626b2aa9477d4f4c0790afd17e4b385e6cd30a0
Baris Balci <barisbalcimusic@gmail.com>
2024-10-25 10:31:35 +0200

    create footer-bottom & change import of icons

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import {
+  faFacebook,
+  faSquareInstagram,
+  faSquareWhatsapp,
+  faTiktok,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <section id="footer-top">
+        <ul>
+          <li>Über uns</li>
+          <li>Kontakt</li>
+          <li>Versand und Lieferung</li>
+          <li>Zahlungsarten</li>
+          <li>Rücksendungen und Umtausch</li>
+        </ul>
+      </section>
+      <section
+        id="footer-bottom"
+        className="flex flex-col items-center gap-[4vw] bg-[#585858] py-[6vw] text-white">
+        <section className="flex flex-col justify-center">
+          <div>
+            <p className="font-bold text-[3vw]">Follow us:</p>
+            <div className="flex gap-[5vw] text-[10vw]">
+              <FontAwesomeIcon className="w-[7vw]" icon={faTiktok} />
+              <FontAwesomeIcon className="w-[7vw]" icon={faFacebook} />
+              <FontAwesomeIcon className="w-[7vw]" icon={faSquareInstagram} />
+              <FontAwesomeIcon className="w-[7vw]" icon={faSquareWhatsapp} />
+              <FontAwesomeIcon className="w-[7vw]" icon={faTwitter} />
+            </div>
+          </div>
+        </section>
+        <ul className="flex justify-center items-center gap-[4vw] font-bold text-white text-[3vw]">
+          <li>AGB</li>|<li>Datenschutz</li>|<li>Impressum</li>
+        </ul>
+        <small className="text-[2vw]">
+          © 2024 Barış Balcı. Alle Rechte vorbehalten.
+        </small>
+      </section>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,35 +1,37 @@
 import { Link } from "react-router-dom";
-import searchIcon from "../assets/icons/magnifying-glass-solid.svg";
-import userIcon from "../assets/icons/user-regular.svg";
-import wishlistIcon from "../assets/icons/heart-regular.svg";
-import cartIcon from "../assets/icons/bag-shopping-solid.svg";
-import menuIcon from "../assets/icons/bars-solid.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBagShopping,
+  faBars,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faUser } from "@fortawesome/free-regular-svg-icons";
 
 const Header = () => {
   return (
     <header className="w-full h-[10vh] bg-white flex items-center shadow-md">
       <nav className="w-full flex justify-between">
         <div className="flex justify-center items-center gap-[4vw] ml-[4vw]">
-          <img src={menuIcon} className="h-[7vw]" alt="menu-icon" />
+          <FontAwesomeIcon className="h-[7vw]" icon={faBars} />
           <h1 className="text-[6vw] mt-[1vw]">BuyTheWay</h1>
         </div>
         <ul className="flex items-center gap-[5vw] justify-end p-[3vw]">
           <li>
-            <img className="h-[5vw]" src={searchIcon} alt="search-icon" />
+            <FontAwesomeIcon className="h-[5vw]" icon={faMagnifyingGlass} />
           </li>
           <li>
             <Link to="/account">
-              <img className="h-[5vw]" src={userIcon} alt="user-icon" />
+              <FontAwesomeIcon className="h-[5vw]" icon={faUser} />
             </Link>
           </li>
           <li>
             <Link to="/wishlist">
-              <img className="h-[5vw]" src={wishlistIcon} alt="wishlist-icon" />
+              <FontAwesomeIcon className="h-[5vw]" icon={faHeart} />
             </Link>
           </li>
           <li>
             <Link to="/cart">
-              <img className="h-[5vw]" src={cartIcon} alt="cart-icon" />
+              <FontAwesomeIcon className="h-[5vw]" icon={faBagShopping} />
             </Link>
           </li>
         </ul>
