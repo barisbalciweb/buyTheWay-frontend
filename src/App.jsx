961a386cf1e53c947b104158259f852aced8c9bc
Baris Balci <barisbalcimusic@gmail.com>
2024-11-07 09:54:56 +0100

    add scrolling to top on page change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import routes from "./routers/routes";
 import Layout from "./layout/Layout";
+import ScrollToTop from "./components/ScrollTop";
 
 const App = () => {
   return (
     <div id="container" className="w-full h-full bg-">
       <Layout>
+        <ScrollToTop />
         <Routes>
           {routes.map((route) => {
             return (
