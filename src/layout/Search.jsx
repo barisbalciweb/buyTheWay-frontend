f5d7ec6006b17f10e65d9af96fb706f26aa8c3af
Baris Balci <barisbalcimusic@gmail.com>
2024-12-09 00:13:28 +0100

    improve styling if Search

--- src/layout/Search.jsx
+++ src/layout/Search.jsx
@@ -10,7 +10,7 @@ import {
   fetchSearchResults,
 } from "../features/search/searchSlice";
 import { Link, useNavigate } from "react-router-dom";
-import { ClipLoader } from "react-spinners";
+import { BeatLoader } from "react-spinners";
 
 const Search = () => {
   const dispatch = useDispatch();
@@ -67,17 +67,16 @@ const Search = () => {
       id="search-main-div"
       className="w-full h-full fixed top-header z-40 px-[5vw] bg-white">
       {/* SEARCH INPUT */}
-      <div
-        id="search-input-div"
-        className="w-full flex flex-col justify-center items-end gap-[4vw] mt-[4vw]">
-        <div className="w-full flex justify-between items-center">
-          <h1 className="text-[7vw] font-bold">Suche</h1>
-          <FontAwesomeIcon
-            icon={faXmark}
-            className="text-[8vw]"
-            onClick={() => dispatch(toggleSearch())}
-          />
-        </div>
+      <div className="w-full h-[7vw] navigation-div flex justify-between items-center mt-[4vw]">
+        <h1 className="text-[7vw] font-bold">Suche</h1>
+        <FontAwesomeIcon
+          icon={faXmark}
+          className="text-[8vw]"
+          onClick={() => dispatch(toggleSearch())}
+        />
+      </div>
+      <div id="search-input-div" className="w-full gap-[4vw] mt-[4vw]">
+        <div className="w-full flex justify-between items-center"></div>
         <label
           htmlFor="search-input"
           className="w-full h-[12vw] flex items-center justify-end relative">
@@ -112,13 +111,13 @@ const Search = () => {
       {/* SEARCH RESULTS */}
       <div
         id="search-results-div"
-        className="w-full h-[calc(100%-17vw-12vw)] py-[5vw] overflow-y-auto">
+        className="w-full h-[calc(100%-17vw-12vw-7vw-4vw-4vw-5vw)] pb-[5vw] mt-[5vw] overflow-y-auto">
         <ul className="w-full flex flex-col justify-center gap-[3vw]">
           {
             // IF LOADING, SHOW SPINNER
             isLoading ? (
               <div className="w-full h-full flex justify-center items-center">
-                <ClipLoader size={"20vw"} className="" />
+                <BeatLoader size={"2vw"} />
               </div>
             ) : // ELSE IF RESULTS NOT EMPTY, LIST THEM
             searchList.length > 0 ? (
@@ -131,7 +130,9 @@ const Search = () => {
               ))
             ) : (
               // ELSE, SHOW THE FEEDBACK TEXT
-              <p>{inputValue === "" ? "" : "Kein Produkt gefunden"}</p>
+              <p className="text-center">
+                {inputValue === "" ? "" : "Kein Produkt gefunden"}
+              </p>
             )
           }
         </ul>
