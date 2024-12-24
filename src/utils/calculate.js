56c3084f1b85b9a97170da812e2b09b3928275fd
Baris Balci <barisbalcimusic@gmail.com>
2024-12-25 00:00:19 +0100

    update calculate logic for total depending on discount

--- src/utils/calculate.js
+++ src/utils/calculate.js
@@ -1,11 +1,23 @@
+import { calculateDiscountedPrice } from "./calculateDiscountedPrice";
+
 export const calculate = (state) => {
   const cartItemsCount = state.cartItems.reduce(
     (count, item) => count + item.quantity,
     0
   );
-  const total = state.cartItems.reduce(
-    (sum, item) => sum + item.product.price * item.quantity,
-    0
-  );
+  const total = state.cartItems.reduce((sum, item) => {
+    if (item.product.discountPercentage > 0) {
+      return (
+        sum +
+        calculateDiscountedPrice(
+          item.product.price,
+          item.product.discountPercentage
+        ) *
+          item.quantity
+      );
+    } else {
+      return sum + item.product.price * item.quantity;
+    }
+  }, 0);
   return { cartItemsCount, total };
 };
