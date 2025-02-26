8c1335b83244a5d3682cfe756118e40017cfbb3a
Baris Balci <barisbalcimusic@gmail.com>
2025-02-26 20:58:35 +0100

    update warning screen

---
+++ src/components/OpenWindowButton.jsx
@@ -0,0 +1,25 @@
+const OpenWindowButton = () => {
+  const openWindow = () => {
+    const url = "http://localhost:5173";
+    const width = 393;
+    const height = 852;
+    const left = (window.screen.width - width) / 2;
+    const top = (window.screen.height - height) / 2;
+
+    window.open(
+      url,
+      "_blank",
+      `width=${width},height=${height},top=${top},left=${left}`
+    );
+  };
+
+  return (
+    <button
+      onClick={openWindow}
+      className="w-[40%] px-4 py-2 bg-blue-700 text-white rounded hover:bg-blue-500 m-auto">
+      buyTheWay
+    </button>
+  );
+};
+
+export default OpenWindowButton;
